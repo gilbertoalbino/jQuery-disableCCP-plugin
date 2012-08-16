7cc348883a4b5da417c04c9af03c4ca4c0ee3b1a
Gilberto Albino <contato@gilbertoalbino.com>
2012-08-16 18:18:47 -0300

    first commit

--- src/jquery.disableCCP.js
+++ src/jquery.disableCCP.js
@@ -1,8 +1,24 @@
+/*
+ * jQuery Disable Cut Copy Paste Plugin 1.0.0
+ * http://github.com/gilbertoalbino/jquery-disableCCP-plugin
+ * Requires jQuery 1.3
+ *
+ * Copyright 2012, Liuitt.
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.opensource.org/licenses/GPL-2.0
+ */
+ 
+ /**
+  * This is a very basic plugin but very functional
+  * Use it if you want to disable cut, copy and past events
+  * to prevent users from bypassing email or password confirmation
+  */
 (function($){
     $.fn.disableCCP = function() {
         return this.each( function() {
             var $this = $(this);
-            $this.bind("cut copy paste",function(e) {
+            $this.live("cut copy paste",function(e) {
                 e.preventDefault();
             })
             
